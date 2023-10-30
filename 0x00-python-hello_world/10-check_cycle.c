9dbeeb8d45053c8651528759455cc3f9e5071e83
Danielyilma <yilmad752@gmail.com>
2023-10-30 20:41:22 +0300

    python script

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,29 +1,30 @@
 #include "lists.h"
 
 /**
+ * check_cycle - checking cycle in linked list
  *
- * 
- * 
- * 
+ * @list: head node
+ *
+ * Return: integer
  */
 
 int check_cycle(listint_t *list)
 {
-        listint_t *slow, *fast;
+	listint_t *slow, *fast;
 
-        if (list == NULL)
-        return (0);
+	if (list == NULL)
+	return (0);
 
-        slow = list;
-        fast = list;
+	slow = list;
+	fast = list;
 
-        while (fast != NULL)
-        {
-                if (fast == slow)
-                return (1);
+	while (fast != NULL)
+	{
+		if (fast == slow)
+		return (1);
 
-                slow = slow->next;
-                fast = fast->next;
-        }
-        return (0);
+		slow = slow->next;
+		fast = fast->next;
+	}
+	return (0);
 }
