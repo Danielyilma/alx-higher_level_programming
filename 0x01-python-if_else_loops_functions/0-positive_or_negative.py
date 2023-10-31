3d051d6e22c2c96e38209a2bc8f8d958e592827e
Danielyilma <yilmad752@gmail.com>
2023-10-31 16:22:17 +0300

    flow control

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,7 +2,9 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print(f"{number:d} is postive")
+    print(f"{number:d} is positive")
+elif number == 0:
+    print(f"{number:d} is zero")
 else:
     print(f"{number:d} is negative")
 # YOUR CODE HERE
--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,10 +2,14 @@
 import random
 number = random.randint(-10000, 10000)
 last = number % 10
+print(f"Last digit of {number} is ", end="")
+if number < 0:
+    last = int(str(number)[-1])
+    last *= -1
 if last > 5:
-    print(f"Last digit of {number:d} is {last:d} and is greater than 5")
+    print(f"{last} and is greater than 5")
 elif last == 0:
-    print(f"Last digit of {number:d} is {last:d} and is 0")
+    print(f"{last} and is 0")
 elif last < 6 and last != 0:
-    print(f"Last digit of {number:d} is {last:d} and is less than 6 and not 0")
+    print(f"{last} and is less than 6 and not 0")
 # YOUR CODE HERE
