7471e07332f2dbc26a8d95eb2ef87f204bdb182a
Danielyilma <yilmad752@gmail.com>
2023-11-02 20:57:19 +0300

    import and modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,8 @@ from sys import argv
 
 
 def main():
-    print("{} arguments:".format(len(argv) - 1))
+    print("{} arguments{}".format(len(argv) - 1, "."
+                                  if len(argv) == 1 else ":"))
     for i, string in enumerate(argv[1:]):
         print("{0}: {1}".format(i + 1, string))
 
