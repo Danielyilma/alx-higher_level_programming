303a94086b02e8adbc4698612bc0f865a01b2c64
Danielyilma <yilmad752@gmail.com>
2023-11-03 00:10:23 +0300

    import and modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+import hidden_4
+
+
+def main():
+    names = dir(hidden_4)
+    for name in enumerate(names):
+        if name[0:2] != "__":
+            print("{}".format(name))
+
+
+if __name__ == "__main__":
+    main()
