3dbccea1f98f37f000ac4551d9a1bf29d1ba178e
Danielyilma <yilmad752@gmail.com>
2023-11-07 03:04:36 +0300

    data structure - list and cpython

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,16 +1,25 @@
-#include "listobject.h"
-#include "object.h"
+#include "/usr/include/python3.10/Python.h"
+#include <stdio.h>
 
 /**
- * print_python_list_info - info about python list
- * @p: python object
+ * print_python_list_info - python list information in the back
+ *
+ * @p: python object type
+ *
+ * Return nothing
  *
 */
+
+
 void print_python_list_info(PyObject *p)
 {
-	int i, l;
+	PyListObject *list = (PyListObject *) p;
+	int i;
 
-	l = PyList_Size(p)
-	for (i = 0 ; i < l ; i++)
-		printf("[*] Size of the Python List = %d", l);
+	printf("[*] Size of the Python List = %lu\n", list->ob_base.ob_size);
+	printf("[*] Allocated = %lu\n", list->allocated);
+	for (i = 0; i < list->ob_base.ob_size; i++)
+	{
+		printf("Element %d: %s\n", i, list->ob_item[i]->ob_type->tp_name);
+	}
 }
