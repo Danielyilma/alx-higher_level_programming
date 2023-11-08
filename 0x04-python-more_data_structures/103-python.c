fb08b0e8d0e58f26a78ef7cccda817a52e938631
Danielyilma <yilmad752@gmail.com>
2023-11-08 20:03:19 +0300

    more on data structure - set, dictionary

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,56 @@
+#include "/usr/include/python3.10/Python.h"
+
+/**
+ * print_python_list - python list
+ *
+ * @p: python object
+ *
+ * Return: nothing
+*/
+
+void print_python_list(PyObject *p)
+{
+	PyListObject *list = (PyListObject *)p;
+	int i = 0;
+
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %lu\n", list->ob_base.ob_size);
+	printf("[*] Allocated = %lu\n", list->allocated);
+
+	for (i = 0; i < list->ob_base.ob_size; i++)
+	{
+		printf("Element %d: %s\n", i, list->ob_item[i]->ob_type->tp_name);
+	}
+}
+
+/**
+ * print_python_bytes - print python byte information
+ *
+ * @p: python object
+ *
+ * Return: nothing
+*/
+
+void print_python_bytes(PyObject *p)
+{
+	PyBytesObject *bytes = (PyBytesObject *)p;
+	int i = 0;
+	char *byt = bytes->ob_sval;
+	ssize_t len = bytes->ob_base.ob_size;
+
+	printf("[.] bytes object info\n");
+	printf("  size: %lu\n", bytes->ob_base.ob_size);
+	printf("  trying string: %s\n", bytes->ob_sval);
+
+	i = len > 10 ? 10 : len + 1;
+
+	printf("  first %d bytes: ", i);
+	i = 0;
+
+	for (i = 0; i <= len && i < 10; i++)
+	{
+		printf("%02x ", (unsigned char)byt[i]);
+	}
+
+	printf("\n");
+}
--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -1,13 +1,31 @@
-#!/usr/bin/python3
-square_matrix_map = \
-    __import__('101-square_matrix_map').square_matrix_map
+import ctypes
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-new_matrix = square_matrix_map(matrix)
-print(new_matrix)
-print(matrix)
+lib = ctypes.CDLL('./ex.so')
+lib.print_python_list.argtypes = [ctypes.py_object]
+lib.print_python_bytes.argtypes = [ctypes.py_object]
+s = b"Hello"
+lib.print_python_bytes(s);
+b = b'\xff\xf8\x00\x00\x00\x00\x00\x00';
+lib.print_python_bytes(b);
+b = b'What does the \'b\' character do in front of a string literal?';
+lib.print_python_bytes(b);
+l = [b'Hello', b'World']
+lib.print_python_list(l)
+del l[1]
+lib.print_python_list(l)
+l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], b"Holberton", "Betty"]
+lib.print_python_list(l)
+l = []
+lib.print_python_list(l)
+l.append(0)
+lib.print_python_list(l)
+l.append(1)
+l.append(2)
+l.append(3)
+l.append(4)
+lib.print_python_list(l)
+l.pop()
+lib.print_python_list(l)
+l = ["Holberton"]
+lib.print_python_list(l)
+lib.print_python_bytes(l);
