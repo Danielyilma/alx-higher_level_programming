5c4d9ada5207fc727efe9399b83da1035f758833
Danielyilma <yilmad752@gmail.com>
2023-11-08 12:50:30 +0300

    more on data structure - set, dictionary

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@
 
 def roman_to_int(roman_string):
     if roman_string is None or type(roman_string) is not str:
-        return None
+        return 0
     equ = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 100}
     leng = len(roman_string)
     num = 0
