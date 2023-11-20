a49ec9c794027a141760a5fddaae34357bd4725e
Danielyilma <yilmad752@gmail.com>
2023-11-20 20:08:05 +0300

    handling error

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -6,5 +6,5 @@ def safe_function(fct, *args):
     try:
         return fct(args[0], args[1])
     except Exception as ex:
-        sys.stderr.write("Exception: {}\n".format(ex))
+        print("Exception: {}".format(ex), file=sys.stderr)
         return None
