dd9fc491975239906af2f55e614bc04dc64934dd
Danielyilma <yilmad752@gmail.com>
2023-11-28 16:08:21 +0300

    understanding python objects

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
-i = 1
-def magic_string():
-    i = i + 1
-    return (i * 'BestSchool')
+def count(length):
+    def magic_string():
+        nonlocal length
+        result = ""
+        length += 1
+        for i in range(length):
+            result += "BestSchool"
+            if i != length - 1:
+                result += ", "
+        return result
+    return magic_string
+magic_string = count(0)
