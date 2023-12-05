75ed3b486738c12e9afc10b37c9bbaf32d9d2a60
Danielyilma <yilmad752@gmail.com>
2023-12-06 01:01:05 +0300

    file in/out

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 '''adding argument and saving to json file'''
 
+
+import os
 import sys
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
 
 value = []
+if os.path.exists("add_item.json"):
+    value = load_from_json_file("add_item.json")
 for val in sys.argv[1:]:
     value.append(val)
 
