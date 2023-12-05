1315f8a03cd4fd46de006d7c2eeba2ec18577660
Danielyilma <yilmad752@gmail.com>
2023-12-06 01:35:32 +0300

    file in/out

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+'''reading and writing to a file'''
 
 
 def append_after(filename="", search_string="", new_string=""):
+    '''implementation'''
+    with open(filename, "r", encoding="utf-8") as f:
+        lines = f.readlines()
 
-    with open(filename, "a+", encoding="utf-8") as f:
-        line = f.readline()
-        while line:
+    with open(filename, "w", encoding="utf-8") as f:
+        for line in lines:
+            f.write(line)
             if line.find(search_string) != -1:
                 f.write(new_string)
-            line = f.readline()
