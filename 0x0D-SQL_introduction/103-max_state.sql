8529932c0bb7c144c2d0497d74d5c9ab4679917a
Danielyilma <yilmad752@gmail.com>
2024-01-17 20:55:07 +0300

    more on sql queries

---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,6 @@
+-- comment
+
+SELECT state, MAX(value) as max_temp
+FROM temperatures
+GROUP BY state
+ORDER BY state
---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+-- listing all cities in california
+SELECT id, name FROM cities
+WHERE state_id = (
+    SELECT id FROM states
+    WHERE name = 'california'
+)
+ORDER BY id;
+ 
