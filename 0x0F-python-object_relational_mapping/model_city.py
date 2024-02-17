b04dba807a7f95308d172543ca94eedbb26cec74
Danielyilma <yilmad752@gmail.com>
2024-02-17 17:09:03 +0300

    orm queries to manipulate table data in the row

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 '''
-creating city class that is mapped to database
+defining city table using sqlalchemy in mysql database
 '''
 from sqlalchemy import Column, String, Integer, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 from model_state import Base
 
 
 class City(Base):
+    '''city class that is mapped to database'''
     __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True)
