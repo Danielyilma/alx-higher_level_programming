2380ddbbb5646b4d0db7c1c98c67f946c92f2b8c
Danielyilma <yilmad752@gmail.com>
2024-02-17 16:25:06 +0300

    orm queries to manipulate table data in the row

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+creating city table in database and displaying the rows
+'''
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_city import Base, City
+from model_state import State
+import sys
+
+
+def main():
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        )
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base.metadata.create_all(engine)
+    results = session.query(State, City).join(City).order_by(City.id).all()
+
+    for state, city in results:
+        print(
+            '{}: ({}) {}'.format(state.name, city.id, city.name)
+        )
+
+
+if __name__ == '__main__':
+    main()
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+creating city class that is mapped to database
+'''
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from model_state import Base
+
+
+class City(Base):
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
