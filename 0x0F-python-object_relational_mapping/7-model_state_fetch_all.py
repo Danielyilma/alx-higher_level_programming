2f0aa539745a73486296dc7adf227a3918ce4bcc
Danielyilma <yilmad752@gmail.com>
2024-02-17 13:41:32 +0300

    fetching all states table data from the database

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,6 +7,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 def main():
     engine = create_engine(
         '''mysql+mysqldb://{}:{}@localhost:3306/{}'''.format(
