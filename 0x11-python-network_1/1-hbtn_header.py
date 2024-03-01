eefac0dff69bfd6c6d8087ca03fff3da5490089d
Danielyilma <yilmad752@gmail.com>
2024-03-01 20:23:31 +0300

    python networking

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 '''getting the value of the HTTP responce header'''
-import urllib
+from urllib import request
 import sys
 
 def main():
+    """
+    The `main` function takes a URL as a command line argument
+    the value of the "X-Request-Id" header from the response.
+    """
     url = sys.argv[1]
 
-    with urllib.request.urlopen(url) as response:
+    with request.urlopen(url) as response:
         print(response.headers.get("X-Request-Id"))
 
 
