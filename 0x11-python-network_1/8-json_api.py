6cbd5200953622c7befa7f357f114e5e80ae9b17
Danielyilma <yilmad752@gmail.com>
2024-03-01 22:22:20 +0300

    python networking

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,22 +5,22 @@ import sys
 
 
 def main():
-    url = sys.argv[1]
+    url = 'http://0.0.0.0:5000/search_user'
     posted_data = {'q': ""}
-    if len(sys.argv) > 2 and sys.argv[2]:
-        posted_data['q'] = sys.argv[2]
+    if len(sys.argv) > 1 and sys.argv[1]:
+        posted_data['q'] = sys.argv[1]
 
     r = requests.post(url, data=posted_data)
 
     try:
         json = r.json()
-        print(json.get('message'))
         if len(json) == 0:
             print('No result')
         else:
             print(f'[{json.get('id')}] {json.get('name')}')
     except requests.exceptions.JSONDecodeError as e:
         print('Not a valid JSON')
+
 
 if __name__ == '__main__':
     main()
