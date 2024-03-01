885aa998f793b03ee37c94f4ff0c1a6e1fa9bb39
Danielyilma <yilmad752@gmail.com>
2024-03-01 23:46:05 +0300

    python networking

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,19 +4,19 @@ import urllib.request
 
 
 def main():
-	'''This Python script is fetching datausing the
-	 `urlopen` function from the `urllib.request` module.'''
-	url = "https://alx-intranet.hbtn.io/status"
-	req = urllib.request.Request(url)
-	with urllib.request.urlopen(req) as response:
-		data = response.read()
-		print('Body response:')
-		print('\t- type: {}'.format(type(data)))
-		print('\t- content: {}'.format(data))    
-		print('\t- utf8 content: {}'''.format(
-			data.decode("utf-8")	
-		))
+    '''This Python script is fetching datausing the
+     `urlopen` function from the `urllib.request` module.'''
+    url = "https://alx-intranet.hbtn.io/status"
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        data = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(data)))
+        print('\t- content: {}'.format(data))
+        print('\t- utf8 content: {}'''.format(
+            data.decode("utf-8")
+        ))
 
 
 if __name__ == '__main__':
-	main()
+    main()
