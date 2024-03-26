2ccf3e54c457a462ac48466fc6e673228911e53a
Danielyilma <yilmad752@gmail.com>
2024-03-27 02:10:54 +0300

    js request module and object serialization

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+const { request } = require('request');
+
+request(argv[2], (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    console.log('Code: ', response.statusCode);
+  }
+});
