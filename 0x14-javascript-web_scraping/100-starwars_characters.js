cc4966a226f4d98daaf81b1d73a824105a74889d
Danielyilma <yilmad752@gmail.com>
2024-03-27 04:07:39 +0300

    js request module and object serialization

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const { argv } = require('process');
+const request = require('request');
+
+request('https://swapi-api.alx-tools.com/api/films/' + argv[2], (err, response, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const data = JSON.parse(body);
+    data.characters.forEach(link => {
+        request(link, (err, response, body) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            data2 = JSON.parse(body);
+            console.log(data2.name)
+        });
+    });
+  });
